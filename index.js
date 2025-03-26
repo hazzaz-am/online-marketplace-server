b26e6a27be959f29a148f243e88f84714579374c
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-03-26 12:51:27 +0600

    fix: cors url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
 	origin: [
 		"http://localhost:5173",
-		"https://online-marketplace-three.vercel.app",
+		"https://online-marketplace-16cef.web.app",
 	],
 	credentials: true,
 	optionSuccessStatus: 200,
